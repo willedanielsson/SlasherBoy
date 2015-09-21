cac3be84787f3873105f9cf1ae7c37c397817d74
willedanielsson <william.l.danielsson@gmail.com>
2015-09-21 10:26:58 +0200

    Commit that has the data from the other repo

---
+++ Assets/Standard Assets/2D/Scripts/attackTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attackTrigger : MonoBehaviour {
+
+	public int dmg = 20;
+
+	void onTriggerEnter2D(Collider2D col){
+		if (col.isTrigger != true && col.CompareTag ("Enemy")) {
+			col.SendMessageUpwards("Damage", dmg);
+		}
+
+	}
+}
