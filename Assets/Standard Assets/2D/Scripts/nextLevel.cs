67f48260415fe81c9e83a9734c74c71efd551ab6
willedanielsson <william.l.danielsson@gmail.com>
2015-09-21 12:07:43 +0200

    Have a tutorial level and when the player enters a cave, the next level is loaded

---
+++ Assets/Standard Assets/2D/Scripts/nextLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextLevel : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.name == "Player") {
+			Application.LoadLevel("test");
+		}
+	}
+}
