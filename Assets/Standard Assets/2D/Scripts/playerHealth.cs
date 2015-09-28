53b86ec8ed0f06cd0b3cad057b474f83e890ad3d
willedanielsson <william.l.danielsson@gmail.com>
2015-09-28 15:03:34 +0200

    Better but not good display of hp

--- Assets/Standard Assets/2D/Scripts/playerHealth.cs
+++ Assets/Standard Assets/2D/Scripts/playerHealth.cs
@@ -9,6 +9,8 @@ public class playerHealth : MonoBehaviour {
 	public Texture2D bgImage;
 	public Texture2D fgImage;
 
+
+
 	public float healthBarLength;
 
 	void Start(){
@@ -20,20 +22,28 @@ public class playerHealth : MonoBehaviour {
 	}
 
 	void OnGUI(){
+		GUIStyle style = new GUIStyle ("box");
+		style.padding = new RectOffset (0, 0, 0, 0);
+		style.fixedHeight = 0;
+		style.fixedWidth = 0;
+		style.stretchHeight = true;
+		style.stretchWidth = true;
+
 		// Create one Group to contain both images
 		// Adjust the first 2 coordinates to place it somewhere else on-screen
-		GUI.BeginGroup(new Rect(0,0, healthBarLength, 32));
+		GUI.BeginGroup(new Rect(0,0, Screen.width / 2, 32));
 
 			// Draw the background image
-			GUI.Box (new Rect (0, 0, healthBarLength, 32), bgImage);
-
+		GUI.Box (new Rect (0, 0, Screen.width / 2, 32), bgImage);
+//		GUI.color = Color.red;
 		// Create a second Group which will be clipped
 		// We want to clip the image and not scale it, which is why we need the second Group
-		GUI.BeginGroup (new Rect (0,0, curHealth / maxHealth * healthBarLength, 32));
-		GUI.color = Color.red;
+		GUI.BeginGroup (new Rect (0,0, Screen.width / 2, 32));
+
 		// Draw the foreground image
-		GUI.Box (new Rect (0,0,healthBarLength,32), fgImage);
-		
+		GUI.Box (new Rect (0,0,healthBarLength,32), fgImage, style);
+
+
 		// End both Groups
 		GUI.EndGroup ();
 		
@@ -59,3 +69,5 @@ public class playerHealth : MonoBehaviour {
 
 	}
 }
+	
+
